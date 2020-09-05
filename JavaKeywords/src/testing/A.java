785a90c3e373a952466b19e46ea7e499dffcc48b
testingworldnoida <testingworldnoida@gmail.com>
2020-09-05 22:20:33 +0530

    Started Automation Framework Added POM file

---
+++ JavaKeywords/src/testing/A.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class A {
+
+	public static X getData(){
+		X o = new X();
+		return o;
+	}
+	
+}
---
+++ JavaKeywords/src/testing/B.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class B {
+
+   public static void main(String[] args) {
+	A  o = new A();
+	X i =A.getData();
+    i.xclsmethod();
+}
+}
